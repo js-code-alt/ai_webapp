98fbddd3ec52494092fab93d1268de505d145d44
Jonas Schwegler <jonas.c.schwegler@gmail.com>
2024-06-18 17:39:19 +0100

    first itteration of recording

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,34 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def upload_audio
+    Rails.logger.debug "Params: #{params.inspect}"
+    audio = params[:audio]
+    Rails.logger.debug "Audio Params: #{audio.inspect}"
+
+    if audio.present?
+      file_path = Rails.root.join('public', 'uploads', audio.original_filename)
+      Rails.logger.debug "Saving to: #{file_path}"
+
+      File.open(file_path, 'wb') do |file|
+        file.write(audio.read)
+      end
+
+      Rails.logger.debug "File saved successfully."
+      head :ok
+    else
+      Rails.logger.error "Audio file not found in params."
+      head :unprocessable_entity
+    end
+  rescue => e
+    Rails.logger.error "Failed to upload audio: #{e.message}"
+    head :unprocessable_entity
+  end
 end
